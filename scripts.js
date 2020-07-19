6708da6d6a59923a18819cea05af8c84f4e3b3c2
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-07-19 13:11:27 +0200

    .random .length

--- scripts.js
+++ scripts.js
@@ -7,5 +7,6 @@ messages += ` ${userName} this is just another suprise for ${userName}`;
 
 let passphrase = (messages.length);
 
+let randomNumber = Math.floor(Math.random() * 8) + 1;
 
-console.log(passphrase / 2);
+console.log(`${userName} ${randomNumber}`);
