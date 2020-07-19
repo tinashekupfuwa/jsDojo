3f8327ffca6d2f3c229c22794d095cb01f02f98f
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-07-19 11:56:09 +0200

    Variables added with soem string literlsa

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 let userName = prompt(`What is your Name?`)
 
 document.write(`<h1>Hi ${userName}</h1>`);
+
+let messages = `Hi ${userName} , we hope you are prepared for this journey`;
+messages += ` ${userName} this is just another suprise for ${userName}`;
+
+document.write(messages);
