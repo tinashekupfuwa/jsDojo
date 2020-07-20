59c01c061e8ed2fd00659f1c1769dbdd2b22112e
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-07-20 10:53:27 +0200

    added a CSS file

---
+++ app.js
@@ -0,0 +1,14 @@
+// let tries;
+
+
+function randomNumber() {
+  let ranValue = Math.random + 1;
+  return ranValue;
+}
+// let i = 0;
+// while (i < 100) {
+//   document.write(`<span>${randomNumber()}</span>`)
+//   i++
+// }
+
+console.log(randomNumber());
